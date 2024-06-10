815e596543a7f6031151930247b20b1451bf317a
Sivaselvan <mvs@buffalo.edu>
2024-06-10 09:17:32 -0400

    Robust analysis added in StateFedbackController.m

--- Shaker-FinDout-approach/StateFeedbackController.m
+++ Shaker-FinDout-approach/StateFeedbackController.m
@@ -10,7 +10,7 @@ kVS = kVSrot/larm^2; % lb/in
 mVS = IVS/larm^2; % lb-s^2/in
 w = sqrt(kVS/mVS); % rad/s
 f = w/2/pi; % Hz
-zet = 0.02; % let's take 2% damping
+zet = 0.01; % let's take 2% damping
 
 %% Actuator data
 kact = 9300; %lb/in
@@ -40,7 +40,7 @@ ssVS = ss(AVS, BVS, CVS, DVS);
 ssVS.InputName = 'w';
 ssVS.OutputName = 'xVS';
 
-ssController = tunableSS('Controller',4,1,2);
+ssController = tunableSS('Controller',2,1,2);
 ssController.InputName = {'x','F'};
 ssController.OutputName = 'u';
 
@@ -111,3 +111,31 @@ figure(303),
 
 figure(304),
     bode(controller(1), controlConstr)
+
+%% Robustness analysis of passivity (positive realness) of control solution
+betu = ureal('beta',bet,'percentage',10);
+kactu = ureal('kact',kact,'percentage',10);
+du = ureal('d',d,'percentage',10);
+alphu = ureal('alph',alph,'plusminus',[-1 5]);
+taud = 0.001; % sample delay
+
+AHydu = [-betu du; 0 -alphu];
+BHydu = [betu*kactu 0; 0 alphu];
+CHydu = [1 0];
+DHydu = [-kactu 0];
+ssHydu = ss(AHydu, BHydu, CHydu, DHydu);
+ssHydu.InputName = {'x','uv'};
+ssHydu.OutputName = 'F';
+
+% introduce sample delay in the controller and do a pade approximation of
+% the delay
+controllerd = controller;
+controllerd.OutputDelay = taud;
+%controllerd = pade(controllerd, 2);
+
+ssHydCLu = connect(ssHydu, controllerd, 'x', 'F');
+
+[ssHydCLu_samples, sampleValues] = usample(ssHydCLu,10);
+
+figure(401),
+    nyquist(-ssHydCLu_samples/s)
