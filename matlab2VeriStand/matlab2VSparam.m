907de22830aa4eac3cd53ad7ff5f6a67ec324794
Sivaselvan <mvs@buffalo.edu>
2024-06-14 22:59:43 -0400

    Change delimiter from ; to space when writing to Veristand param file, since LabVIEW model was set up with Ac, Bc, Cc and Dc as rows and not columns

--- matlab2VeriStand/matlab2VSparam.m
+++ matlab2VeriStand/matlab2VSparam.m
@@ -1,13 +1,13 @@
-function str = matlab2VSparam(var)
+function str = matlab2VSparam(var,delim)
 
 % input: 
 % var = nx1 vector
 % output
-% str = string: 'varname=[var(1);var(2);...var(n)]'
+% str = string: 'varname=[var(1) delim var(2) delim ...var(n)]'
 
 str = inputname(1);
 str = strcat(str,'=[');
 for n = 1:length(var)-1
-    str = strcat(str,num2str(var(n)),';');
+    str = strcat(str,num2str(var(n)),delim);
 end
 str = strcat(str,num2str(var(end)),']');
--- matlab2VeriStand/writeController2ParamFile.m
+++ matlab2VeriStand/writeController2ParamFile.m
@@ -4,24 +4,26 @@ function writeController2ParamFile(controller, filename)
 
 fid = fopen(filename,'w');
 
+delim = ' '; % space
+
 Ac = controller.A'; % transpose get it to row major form
 Ac = Ac(:);
-str = matlab2VSparam(Ac);
+str = matlab2VSparam(Ac,delim);
 fprintf(fid,'%s\n',str);
 
 Bc = controller.B';
 Bc = Bc(:);
-str = matlab2VSparam(Bc);
+str = matlab2VSparam(Bc,delim);
 fprintf(fid,'%s\n',str);
 
 Cc = controller.C';
 Cc = Cc(:);
-str = matlab2VSparam(Cc);
+str = matlab2VSparam(Cc,delim);
 fprintf(fid,'%s\n',str);
 
 Dc = controller.D';
 Dc = Dc(:);
-str = matlab2VSparam(Dc);
+str = matlab2VSparam(Dc,delim);
 fprintf(fid,'%s\n',str);
 
 fclose(fid);
